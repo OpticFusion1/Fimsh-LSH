228c2821b82de56535f9d59f3584f5fe598989d7
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2024-09-05 20:18:02 -0400

    v0.3.0 Changed prompt symbol to ><> Updated startup message Added command to view current working directory

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -7,23 +7,26 @@
 /*
   Function Declarations for builtin shell commands:
  */
-int lsh_cd(char **args);
+int lsh_swim(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
+int lsh_current(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
  */
 const char *builtin_str[] = {
-    "cd",
+    "swim",
     "help",
-    "exit"
+    "exit",
+    "current"
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
+  &lsh_swim,
   &lsh_help,
-  &lsh_exit
+  &lsh_exit,
+  &lsh_current
 };
 
 int lsh_num_builtins() {
@@ -36,13 +39,13 @@ int lsh_num_builtins() {
 
 /**
    @brief Bultin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @param args List of args.  args[0] is "swim".  args[1] is the directory.
    @return Always returns 1, to continue executing.
  */
-int lsh_cd(char **args)
+int lsh_swim(char **args)
 {
   if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    fprintf(stderr, "lsh: expected argument to \"swim\"\n");
   } else {
     if (chdir(args[1]) != 0) {
       perror("lsh");
@@ -78,6 +81,20 @@ int lsh_help(char **args)
 int lsh_exit(char **args)
 {
   return 0;
+}
+
+int lsh_current(char **args)
+{
+    // Define a buffer to hold the path
+    char cwd[1024];
+    
+    // Get the current working directory
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        printf("Current working directory: %s\n", cwd);
+    } else {
+        //std::cerr << "Error getting current working directory: " << strerror(errno) << std::endl;
+    }
+    return 1;
 }
 
 /**
